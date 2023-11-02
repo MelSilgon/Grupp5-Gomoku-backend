14e142f2cf33a8568ee40ede08aa761856157659
Joacim <18618554+joacimstrandvide@users.noreply.github.com>
2023-11-02 15:44:50 +0100

    Added new routes for all games

--- src/routes/gomoku_routes.js
+++ src/routes/gomoku_routes.js
@@ -1,17 +1,37 @@
 const express = require('express')
 const router = express.Router()
-const gameData = require('./game.json') // adjust the path if the file is in a different directory
+const emptyData = require('./empty_game.json')
+const fullData = require('./full_game.json')
+const whiteData = require('./white_game.json')
+const blackData = require('./black_game.json')
 
 router.get('/create_game', (req, res) => {
-    res.json(gameData)
+    res.json(emptyData)
 })
 
 router.get('/add_player', (req, res) => {
-    res.json(gameData)
+    res.json(emptyData)
 })
 
 router.get('/play', (req, res) => {
-    res.json(gameData)
+    res.json(emptyData)
 })
+
+// Different games
+router.get('/empty', (req, res) => {
+    res.json(emptyData);
+});
+
+router.get('/full', (req, res) => {
+    res.json(fullData);
+});
+
+router.get('/white', (req, res) => {
+    res.json(whiteData);
+});
+
+router.get('/black', (req, res) => {
+    res.json(blackData);
+});
 
 module.exports = router
